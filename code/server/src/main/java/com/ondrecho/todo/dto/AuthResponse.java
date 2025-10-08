f75896a57b0c48ecafa3d15a4dba8175e735aa18
SHAUCHENKA ANDREI <2908ash@gmail.com>
2025-10-08 17:02:01 +0300

    add auth dtos

---
+++ code/server/src/main/java/com/ondrecho/todo/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.ondrecho.todo.dto;
+
+public class AuthResponse {
+    private String token;
+    private Long id;
+    private String username;
+
+    public AuthResponse() {}
+    public AuthResponse(String token, Long id, String username) {
+        this.token = token; this.id = id; this.username = username;
+    }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+}
