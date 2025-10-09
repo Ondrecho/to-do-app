740c0228f0607cbe05191b8ac02eb77d1ac628c0
SHAUCHENKA ANDREI <2908ash@gmail.com>
2025-10-09 20:30:44 +0300

    update auth response

--- code/server/src/main/java/com/ondrecho/todo/dto/AuthResponse.java
+++ code/server/src/main/java/com/ondrecho/todo/dto/AuthResponse.java
@@ -2,17 +2,17 @@ package com.ondrecho.todo.dto;
 
 public class AuthResponse {
     private String token;
-    private Long id;
-    private String username;
+    private UserDto user;
 
     public AuthResponse() {}
     public AuthResponse(String token, Long id, String username) {
-        this.token = token; this.id = id; this.username = username;
+        this.token = token;
+        this.user = new UserDto(id, username, "");
     }
+
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+
+    public UserDto getUser() { return user; }
+    public void setUser(UserDto user) { this.user = user; }
 }
